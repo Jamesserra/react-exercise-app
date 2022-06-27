447881baaeaa396a48c23b82039fa77af4a6a7b5
jamesserra <jamesserra4@gmail.com>
2022-06-27 10:53:43 -0400

    pages

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { AddBox } from "@mui/icons-material";
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>
-      Exercises<div></div>
-    </div>
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h4">Showing Results</Typography>
+    </Box>
   );
 };
 
